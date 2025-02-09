7aa579f3131e7369a282aced06883e8623afdcba
sumit.sharma <sumit72sharma99@gmail.com>
2025-02-10 00:01:46 +0530

    Commit & Push by Sumit on 10-02-2025

--- src/SEPT24/Eg_170924_IfElse/Lab22_PrimeNumber.java
+++ src/SEPT24/Eg_170924_IfElse/Lab22_PrimeNumber.java
@@ -4,10 +4,24 @@ import java.util.Scanner;
 
 public class Lab22_PrimeNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter the No to Check");
-        int no = sc.nextInt();
-
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the no to check");
+        int flag=0;
+        int num = s.nextInt();
+        for( int i=2 ; i<=Math.sqrt(num) ; i++)
+        {
+            if((num % i)==0)
+            {
+                System.out.println("No is not Prime");
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            System.out.println("No is Prime" );
+        }
+        s.close();
 
     }
 
